333d84359b5cca32c5f36730580c69190e95dad4
Marc <konopka.marc@web.de>
2026-04-02 21:27:13 +0200

    status added

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,17 +1,9 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject{  
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
-
     lastHit = 0;
 
     applyGravity() {
@@ -27,33 +19,6 @@ class MovableObject {
         return this.y < 174;
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
--- models/world.class.js
+++ models/world.class.js
@@ -6,6 +6,7 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -25,7 +26,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
                     this.character.hit();
-                    console.log('Collision with Character, energy', this.character.energy);
+                    this.statusBar.setPercentage(this.character.energy )
 
                 }
             })
@@ -38,6 +39,12 @@ class World {
         this.ctx.translate(this.camera_x, 0)
 
         this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0) // Back
+        this.addToMap(this.statusBar);  
+        this.ctx.translate(this.camera_x, 0) // Forwards
+
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
